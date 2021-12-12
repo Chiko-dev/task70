1cee9f631b7011ba8598fb5286189332f33237cf
Chiko-Dev <chikobg12345@gmail.com>
2021-12-12 21:27:55 +0200

    JS

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,33 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const selectors = { 
+    profile: ".profile",
+    profileImageHolder: ".image"
+  }
+
+  Class Profile {
+    constructor(holder){
+      this.profile = holder.querySelector(selectors.profile);
+
+      this.init();
+    }
+
+    init(){
+      this.expandImage();
+    }
+
+    expandImage(){
+    this.profile.addEventListener("mouseover", () => {
+      this.profile.classList.toggle("active");
+    });
+    }
+  }
+
+  document.querySelectorAll(selectors.profileImageHolder).forEach(holder =>{
+    new Profile(holder);
+  });
+
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
